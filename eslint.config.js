f44560716a5a15ac24084490f10d22e9120a92bb
sarakthon <189987357+sarakthon@users.noreply.github.com>
2025-09-02 21:53:35 +0200

    Disable import/consistent-type-specifier-style rule as it caused issues with vite

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
-import { default as defaultConfig } from '@epic-web/config/eslint'
+import { default as defaultConfig } from "@epic-web/config/eslint";
 
 /** @type {import("eslint").Linter.Config} */
 export default [
-	...defaultConfig,
-	// add custom config objects here:
-	{
-		files: ['**/tests/**/*.ts'],
-		rules: { 'react-hooks/rules-of-hooks': 'off' },
-	},
-	{
-		ignores: ['.react-router/*'],
-	},
-]
+  ...defaultConfig,
+  // add custom config objects here:
+  {
+    files: ["**/*.{js,jsx,ts,tsx,mjs,cjs}"],
+    rules: {
+      "import/consistent-type-specifier-style": ["off"],
+    },
+  },
+  {
+    ignores: [".react-router/*"],
+  },
+];

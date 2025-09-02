09c973765e46a1cee767f8a242fb1956b185f83e
sarakthon <189987357+sarakthon@users.noreply.github.com>
2025-09-02 23:31:33 +0200

    Create shared code for everything "app config" related, and improve developer experience by checking environment variables at startup using zod

---
+++ app/lib/appConfig.ts
@@ -0,0 +1,39 @@
+import z, { type ZodError } from "zod";
+
+const appConfigSchema = z.object({});
+export type AppConfig = z.infer<typeof appConfigSchema>;
+
+const appSecretsSchema = z.object({
+  DATABASE_URL: z.string(),
+});
+export type AppSecrets = z.infer<typeof appSecretsSchema>;
+
+export function getAppConfig() {
+  try {
+    const config = appConfigSchema.parse(process.env);
+    return config;
+  } catch (error) {
+    throw new Error(
+      `Required environment variables not set correctly: ${(
+        error as ZodError
+      ).issues
+        .map((i) => `${i.path} (${i.message})`)
+        .join(", ")}`
+    );
+  }
+}
+
+export function getAppSecrets() {
+  try {
+    const config = appSecretsSchema.parse(process.env);
+    return config;
+  } catch (error) {
+    throw new Error(
+      `Required environment variables not set correctly: ${(
+        error as ZodError
+      ).issues
+        .map((i) => `${i.path} (${i.message})`)
+        .join(", ")}`
+    );
+  }
+}

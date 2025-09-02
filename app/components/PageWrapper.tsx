d2b3649b05459696f03d1c86c45cb7ec154bdd46
sarakthon <189987357+sarakthon@users.noreply.github.com>
2025-09-02 22:41:36 +0200

    Create a more responsive navigation component for both mobile and desktop * Also fix issue in dev tooling so that we can visit my mac (tott.local) from my iPhone

--- app/components/PageWrapper.tsx
+++ app/components/PageWrapper.tsx
@@ -1,15 +1,151 @@
-import { Link } from "react-router";
+import { FileTextIcon, Pencil2Icon, PersonIcon } from "@radix-ui/react-icons";
+import { NavLink } from "react-router";
 
 export function PageWrapper({ children }: { children: React.ReactNode }) {
-    return <main className="max-w-[900px] mx-auto">
-        <ul className="flex gap-4 py-2 bg-amber-100 px-4 rounded-2xl mt-2">
-            <li><Link to="/">Hjem</Link></li>
-            <li><Link to="/about">Om prosjektet</Link></li>
-            <li><Link to="/recipes">Oppskrifter</Link></li>
-            <li><Link to="/projects">Prosjekter</Link></li>
-            <li><Link to="/new_project">Ny oppskrift</Link></li>
-            <li><Link to="/delete_project">Slett oppskrift</Link></li>
-        </ul>
-        {children}
+  return (
+    <main className="max-w-[900px] mx-auto">
+      <TopMenuBar className="hidden md:flex" />
+      <MobileBottomMenuBar className="flex md:hidden" />
+      {children}
     </main>
+  );
+}
+
+function TopMenuBar({ className = "" }: { className?: string }) {
+  return (
+    <div
+      className={`flex justify-between items-center mb-12 px-8 py-2 mt-2 bg-white shadow-lg rounded-full ${className}`}
+    >
+      <div className="font-bold text-black texttext-slate-600-2xl">
+        Min Strikkeapp
+      </div>
+      <ul className="flex gap-8">
+        <li>
+          <NavLink
+            to="/recipes"
+            className={({ isActive, isPending }) => {
+              return `flex gap-1 items-center py-2 text-sm ${isActive ? "text-black" : "text-slate-600"}`;
+            }}
+          >
+            {({ isActive, isPending }) => {
+              return (
+                <>
+                  <FileTextIcon
+                    className="size-5"
+                    color={isActive ? "#000000" : "#999999"}
+                  />
+                  Oppskrifter
+                </>
+              );
+            }}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/projects"
+            className={({ isActive, isPending }) => {
+              return `flex gap-1 items-center py-2 text-sm ${isActive ? "text-black" : "text-slate-600"}`;
+            }}
+          >
+            {({ isActive, isPending }) => {
+              return (
+                <>
+                  <Pencil2Icon
+                    className="size-5"
+                    color={isActive ? "#000000" : "#999999"}
+                  />
+                  Prosjekter
+                </>
+              );
+            }}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/profile"
+            className={({ isActive, isPending }) => {
+              return `flex gap-1 items-center py-2 text-sm ${isActive ? "text-black" : "text-slate-600"}`;
+            }}
+          >
+            {({ isActive, isPending }) => {
+              return (
+                <>
+                  <PersonIcon
+                    className="size-5"
+                    color={isActive ? "#000000" : "#999999"}
+                  />
+                  Profil
+                </>
+              );
+            }}
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function MobileBottomMenuBar({ className = "" }: { className?: string }) {
+  return (
+    <div
+      className={`fixed bottom-3 rounded-full bg-white px-4 left-12 right-12 shadow-2xl border-[1px] border-gray-200 ${className}`}
+    >
+      <div className="grid grid-cols-3 w-full gap-4">
+        <NavLink
+          to="/recipes"
+          className={({ isActive, isPending }) => {
+            return `flex flex-col gap-1 items-center py-2 text-[11px] ${isActive ? "font-bold text-black" : "text-slate-600"}`;
+          }}
+        >
+          {({ isActive, isPending }) => {
+            return (
+              <>
+                <FileTextIcon
+                  className="size-5"
+                  color={isActive ? "#000000" : "#999999"}
+                />
+                Oppskrifter
+              </>
+            );
+          }}
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive, isPending }) => {
+            return `flex flex-col gap-1 items-center py-2 text-[11px] ${isActive ? "font-bold text-black" : "text-slate-600"}`;
+          }}
+        >
+          {({ isActive, isPending }) => {
+            return (
+              <>
+                <Pencil2Icon
+                  className="size-5"
+                  color={isActive ? "#000000" : "#999999"}
+                />
+                Prosjekter
+              </>
+            );
+          }}
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive, isPending }) => {
+            return `flex flex-col gap-1 items-center py-2 text-[11px] ${isActive ? "font-bold text-black" : "text-slate-600"}`;
+          }}
+        >
+          {({ isActive, isPending }) => {
+            return (
+              <>
+                <PersonIcon
+                  className="size-5"
+                  color={isActive ? "#000000" : "#999999"}
+                />
+                Profil
+              </>
+            );
+          }}
+        </NavLink>
+      </div>
+    </div>
+  );
 }

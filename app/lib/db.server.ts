22339f078fb9632214dbd7b7674c876dd3dc5885
sarakthon <189987357+sarakthon@users.noreply.github.com>
2025-09-05 21:13:23 +0200

    Allow self signed certificates as digital ocean uses this for their dev databases

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,4 +1,5 @@
 import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 declare global {
   // Allow global caching in dev to avoid creating many pools on HMR
@@ -7,7 +8,13 @@ declare global {
 
 export function getDb(database_url: string) {
   if (global.__db == null) {
-    global.__db = drizzle(database_url);
+    const pool = new Pool({
+      connectionString: database_url,
+      ssl: {
+        rejectUnauthorized: false, // This allows self-signed certificates
+      },
+    });
+    global.__db = drizzle(pool);
   }
   return global.__db;
 }

856995f28de04e52fe3ef35d8b501378b43c090c
sarakthon <189987357+sarakthon@users.noreply.github.com>
2025-09-05 21:20:53 +0200

    Also allow self signed certificates during migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    out: './drizzle',
-    schema: './app/db/schema.ts',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
+  out: "./drizzle",
+  schema: "./app/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    ssl: {
+      rejectUnauthorized: false,
     },
+  },
 });
